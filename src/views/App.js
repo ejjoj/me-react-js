b6db507d1ea3330fb7e0601f8d188d60c0843c93
ejjoj <szymaskibartosz@gmail.com>
2020-09-19 14:48:12 +0200

    added structure

---
+++ src/views/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "../styles/App.scss";
+
+class App extends Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default App;
